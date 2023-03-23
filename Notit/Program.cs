4e3a521068009ef06458ccc572948487c1ca1db3
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-03-23 11:35:12 +0100

    Renamed projects

--- Notit/Program.cs
+++ Notit/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Notit;
+using Notit.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
