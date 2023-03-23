47b78929c61355fd984b2950c1c6e45101585ff0
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-03-23 11:36:26 +0100

    Added Thread and Comment objects

---
+++ Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Notit.Shared.Models
+{
+    public class Comment
+    {
+        public Comment(string text, string user) 
+        { 
+            Text = text;
+            User = user;
+        }
+
+        public long CommentId { get; set; }
+        public string Text { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ Thread.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Notit.Shared.Models;
+
+namespace Notit.Shared.Models
+{
+    public class Thread
+    {
+        public Thread(string content, string user)
+        {
+            Content = content;
+            User = user;
+        }
+
+        public long ThreadId { get; set; }
+        public string Content { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public int Score { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
