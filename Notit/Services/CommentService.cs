d0177ede3b139810f2f1a454699ed65910b9c86b
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-03-24 03:50:59 +0100

    Initial services commit for client

---
+++ Notit/Services/CommentService.cs
@@ -0,0 +1,21 @@
+﻿using Notit.Shared.Models;
+using System.Net.Http.Json;
+
+namespace Notit.Client.Services
+{
+    public class CommentService
+    {
+        private readonly HttpClient httpClient;
+
+        public CommentService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public Task<Comment> GetComment(int commentId)
+        {
+            var result = httpClient.GetFromJsonAsync<Comment>($"api/comment?id={commentId}");
+            return result;
+        }
+    }
+}
---
+++ Notit/Services/ThreadService.cs
@@ -0,0 +1,22 @@
+﻿using Notit.Shared.Models;
+using System.Net.Http.Json;
+using Thread = Notit.Shared.Models.Thread;
+
+namespace Notit.Client.Services
+{
+    public class ThreadService
+    {
+        private readonly HttpClient httpClient;
+
+        public ThreadService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public Task<Thread> GetThread (int threadId) 
+        {
+            var result = httpClient.GetFromJsonAsync<Thread>($"api/thread?id={threadId}");
+            return result;
+        }
+    }
+}
