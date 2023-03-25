6913b1d241412eab8b38606f505114116bda1c21
Magnus Overgård <magnus.overgaard1711@gmail.com>
2023-03-25 05:59:07 +0100

    Initial controller structure

---
+++ Notit-API/Controllers/CommentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notit.Shared.Models;
+
+namespace Notit.API.Controllers
+{
+    [Route("api/comment")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        [HttpGet]
+        public Comment Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public void Post(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public void Put(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Notit-API/Controllers/ThreadController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notit.Shared.Models;
+using Thread = Notit.Shared.Models.Thread;
+
+namespace Notit.API.Controllers
+{
+    [Route("api/thread")]
+    [ApiController]
+    public class ThreadController : ControllerBase
+    {
+        [HttpGet]
+        public Thread Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public void Post(Thread thread)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public void Put(Thread thread)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("comments")]
+        public List<Comment> GetComments(long threadid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
